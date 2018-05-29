9518e16e2fe8e0b9f8247d5bb89b244823a52045
furukami <soranguriarumika@gmail.com>
2018-05-29 12:19:09 +0900

    Add files via upload

---
+++ 16j5027_tcp_c_20180529.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void ErrorMT(char *str){
+	fprintf(stderr,"Faild to %s\n",str);
+	exit(1);
+}
+
+int main(int argc,char *argv[]){
+	int sock;
+	char *ipadd;
+	int Portnum;
+	struct sockaddr_in server;
+	struct sockaddr_in client;
+	//	char buf[1024] = "hoge";
+	int rm;
+	int conn;
+	int byte;
+	unsigned int len;
+
+	if(argc != 3){
+		fprintf(stderr,"Uses : ./client IPaddress Portnumver\n");
+		exit(1);
+	}
+
+	ipadd = argv[1];
+	Portnum = atoi(argv[2]);
+
+	memset(&server,0,sizeof(server));
+	server.sin_family = AF_INET;
+	//	printf("%s,",ipadd);
+	server.sin_addr.s_addr = inet_addr(ipadd);
+	//	printf("%d,",Portnum);
+	server.sin_port = htons(Portnum);
+
+	if((sock = socket(AF_INET,SOCK_STREAM,0)) < 0){
+		ErrorMT("socket");
+	}
+	if(conn = connect(sock,(struct sockaddr *)&server,sizeof(server)) < 0){
+		ErrorMT("connect");
+	}
+	while(1){
+		int string_point = 0;
+		char buf[1024];
+		char rebuf[1024];
+		char catch[1024];
+		fprintf(stdout,"What do you sent word?\n");
+		if(fgets(catch,sizeof(buf),stdin) == NULL){
+			ErrorMT("input");
+			//fprintf(stderr,"Faild to input\n");
+		}
+		if(buf[0] == '-'){
+			if(buf[1] == 'c'){
+				break;
+			}
+		}
+		while(catch[string_point] != '\0'){
+			if(catch[string_point] == '\n'){
+				buf[string_point] = '\0';
+			}else{
+				buf[string_point] = catch[string_point];
+			}
+			string_point++;
+		}
+		byte = strlen(buf)+1;
+		//sscanf(buf,"%s",buf);
+
+		if(send(sock,buf,byte,0) < 0){
+			ErrorMT("sent");
+		}
+		if((rm = recv(sock,rebuf,1024,0)) < 0){
+			ErrorMT("reception");
+		}
+
+		printf("received %s\n",rebuf);
+	}
+	close(sock);
+
+	return 0;
+}
+/*
+   16j5027@lin3441:~/Cnetwork$ ./client 160.194.128.163 5000
+   What do you sent word?
+   HOGEHOGE
+   received hogehoge
+
+   16j5027@lin3441:~/Cnetwork$
+ */
---
+++ 16j5027_tcp_s_20180529.c
@@ -0,0 +1,95 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#include<sys/socket.h>
+#include<netinet/in.h>
+
+void ErrorMT(char *str){
+	fprintf(stderr,"Falild to %s\n",str);
+	exit(1);
+}
+
+int main(int argc , char *argv[]){
+	int sock;
+	char *ipadd;
+	int Portnum;
+	struct sockaddr_in server;
+	struct sockaddr_in client;
+	int rm;
+	int byte;
+	int i;
+	unsigned int len;
+	int s;
+
+	if(argc != 3){
+		fprintf(stderr,"Uses : ./server IPaddress Portnumber\n");
+		exit(1);
+	}
+	ipadd = argv[1];
+	Portnum = atoi(argv[2]);
+
+
+	memset(&server,0,sizeof(server));
+	server.sin_family = AF_INET;
+	//server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_addr.s_addr = inet_addr(ipadd);
+	//server.sin_port = htons(5000);
+	server.sin_port = htons(Portnum);
+
+	if((sock = socket(AF_INET,SOCK_STREAM,0)) < 0){
+		ErrorMT("socket");
+	}
+	if(bind(sock,(struct sockaddr *)&server,sizeof(server)) < 0){
+		ErrorMT("bind");
+	}
+	if(listen(sock,5) < 0){
+		ErrorMT("listen");
+	}
+	while(1){
+		if((s = accept(sock,(struct sockaddr *)&client,&len)) < 0){
+			ErrorMT("accept");
+		}
+		while(1){
+			char buf[1024];
+			/*
+			if((rm = recv(s,buf,1024,0)) <= 0){
+				ErrorMT("recv");
+			}
+			*/
+			for(i = 0;i < 1024;i++){
+				if(rm = recv(s,&buf[i],1,0) <= 0){
+					printf("%c\n",buf[i]);
+					break;
+				}
+			}
+			if(buf[0] == '-'){
+				if(buf[1] == 'c'){
+					close(s);
+				}
+			}
+			printf("received %s\n",buf);
+			i = 0;
+			char *p = buf;
+			printf("%c\n",*p);
+			char rebuf[1024];
+			while(*p != '\0'){
+				rebuf[i] = (char)tolower(*p);
+				i++;
+				p++;
+			}
+			byte = strlen(rebuf)+1;
+			//		fprintf(stdout,"%s",rebuf);
+			if(send(s,rebuf,byte,0) <= 0){
+				ErrorMT("send");
+			}
+		}
+
+	}
+	if(close(sock) < 0){
+		ErrorMT("close");
+	}
+
+	return 0;
+}
